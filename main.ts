bba6d98795cc2f937643963a9e58ab3cbb779e56
asimove <pascal.bras@gmail.com>
2020-12-10 17:40:04 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(1, 6))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
